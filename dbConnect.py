81bc85be39725e258f64f3e25bfd83a9a8d7ba0b
Erion Metolli <erion1metolli@gmail.com>
2016-11-08 01:50:59 +0100

    Removed the unused files

--- dbConnect.py
+++
@@ -1,9 +0,0 @@
-# Made for security purposes because of the credentials
-import PDatabase
-
-def connect():
-    db = PDatabase.PDatabase("databasename", "username", "host", "password")
-    if db.connected:
-        return True
-    else:
-        return False
